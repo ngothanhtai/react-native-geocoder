1e1c08cd1fbf2d5bc552242f9b8ec6d1f59bfa54
Ngo Thanh Tai <ngothanhtai89@yahoo.com>
2017-03-23 12:22:37 +0700

    Config to use only Google API

--- js/geocoder.js
+++ js/geocoder.js
@@ -14,21 +14,23 @@ export default {
     if (!position || !position.lat || !position.lng) {
       return Promise.reject(new Error("invalid position: {lat, lng} required"));
     }
+    return GoogleApi.geocodePosition(this.apiKey, position);
 
-    return RNGeocoder.geocodePosition(position).catch(err => {
-      if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
-      return GoogleApi.geocodePosition(this.apiKey, position);
-    });
+    // return RNGeocoder.geocodePosition(position).catch(err => {
+    //   if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+    //   return GoogleApi.geocodePosition(this.apiKey, position);
+    // });
   },
 
   geocodeAddress(address) {
     if (!address) {
       return Promise.reject(new Error("address is null"));
     }
+    return GoogleApi.geocodeAddress(this.apiKey, address);
 
-    return RNGeocoder.geocodeAddress(address).catch(err => {
-      if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
-      return GoogleApi.geocodeAddress(this.apiKey, address);
-    });
+    // return RNGeocoder.geocodeAddress(address).catch(err => {
+    //   if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+    //   return GoogleApi.geocodeAddress(this.apiKey, address);
+    // });
   },
 }
